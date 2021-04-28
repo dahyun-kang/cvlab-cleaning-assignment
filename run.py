ed8bbe5a5a3df601604bf70b5bd74cec5df4796d
dahyun-kang <dahyun.kang@postech.ac.kr>
2021-04-28 10:48:58 +0900

    Add comments

--- run.py
+++ run.py
@@ -16,44 +16,66 @@ def fill_str(input_s="", max_size=70, fill_char=" "):
 
 class CVLabCleaningAssignment:
     def __init__(self):
-        self.students = self._init_students()
-        self.regions = self._init_regions()
+        self.all_students = self._init_students()
+        self.all_areas = self._init_area()
 
     def _init_students(self):
         students = []
         with open ('seats.csv') as f:
             for sector in f:
                 sector = sector.strip().split(',')
-                if len(sector) > 3:  # few-student sector is ignored
+                if len(sector) > 3:  # few-student sectors are ignored
                     students.append(sector)
         return students
 
-    def _init_regions(self):
-        regions = dict()
-        regions["공용 공간 바닥, 공용 공간 책상, 소파 밑 먼지"] = 3
-        regions["왼쪽 입구쪽 복도, 신발장, 옷장"] = 2
-        regions["오른쪽 입구쪽 복도, 신발장, 옷장"] = 2
-        regions["냉장고 정리 (유통기한 지난 것들, 이름 없는 것들)"] = 2
-        regions["전자레인지 닦기, 소파 먼지 털고 오기"] = 2
-        regions["파쇄기 비우기/복합기 옆 종이 버리기, 대청소 이후 청소기 잔여물 제거"] = 2
-        regions["왼/오른쪽 창가 정리, 창가쪽 복도 닦기"] = 2
-        return regions
+    def _init_area(self):
+        '''
+        Initialize a dictionary of (area -> required number of people)
+        '''
+
+        area = dict()
+        area["공용 공간 바닥, 공용 공간 책상, 소파 밑 먼지"] = 3
+        area["왼쪽 입구쪽 복도, 신발장, 옷장"] = 2
+        area["오른쪽 입구쪽 복도, 신발장, 옷장"] = 2
+        area["냉장고 정리 (유통기한 지난 것들, 이름 없는 것들)"] = 2
+        area["전자레인지 닦기, 소파 먼지 털고 오기"] = 2
+        area["파쇄기 비우기/복합기 옆 종이 버리기, 대청소 이후 청소기 잔여물 제거"] = 2
+        area["왼/오른쪽 창가 정리, 창가쪽 복도 닦기"] = 2
+        return area
 
     def assign(self):
-        random.shuffle(self.students)  # shuffle sector
-        for region, students_subset in zip(self.regions, self.students):
-            random.shuffle(students_subset)  # shuffle priority
-            num = self.regions[region]
-            print(f'{fill_str(region)} | {students_subset[:num]}')  # slice students
-        print(f'{fill_str("커피머신 청소")} | {["민교"]}')  # hardcoded assignment
-        print(f'{fill_str("본인 섹터 청소, 개인 공간 청소, 개인 휴지통 비우기")} | {"나머지"}')  # hardcoded assignment
+        assignments = []
+
+        ''' shuffle sectors '''
+        random.shuffle(self.all_students)
+
+        for area, students in zip(self.all_areas, self.all_students):
+            ''' shuffle students in a sector '''
+            random.shuffle(students)
+
+            ''' assign required number of students at the area '''
+            num = self.all_areas[area]
+            sampled_students = students[:num]
+            assignments.append((area, sampled_students))
+
+        ''' hardcoded assignments '''
+        assignments.append(("커피머신 청소", ["민교"]))
+        assignments.append(("본인 섹터 청소, 개인 공간 청소, 개인 휴지통 비우기", ["나머지"]))
+
+        self.print(assignments)
+
+    def print(self, assignments):
+        print(f"\n{args.date} 차 공용 구역 청소 당번입니다.\n")
+        for k, v in assignments:
+            print(f'{fill_str(k)} | {v}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-date', type=int, help='Random seed in a form of YYYYMM', default=202103)
     args = parser.parse_args()
+
+    ''' set a random seed as the integer YYYYMM '''
     random.seed(args.date)
 
-    print(f"\n{args.date} 차 공용 구역 청소 당번입니다.\n")
     CVLabCleaningAssignment().assign()
